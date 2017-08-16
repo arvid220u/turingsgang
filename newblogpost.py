44d794c7f2e6ae1a7b91b16181e8e88546088540
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2017-08-16 18:50:30 +0000

    Script to create new blog posts

---
+++ newblogpost.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from datetime import datetime
+
+import os, sys
+name = input()
+if len(name.split()) != 1:
+    print("must be onw word")
+    sys.exit()
+
+if os.path.exists("blog/" + name):
+    print("Potentially dangerous  – blogid already taken. Continue? (y/n)")
+    ans = input()
+    if ans != "y" and ans != "Y":
+        sys.exit()
+else:
+    os.makedirs("blog/" + name)
+
+with open("blog/" + name + "/content.html", "w") as cf:
+    cf.write("blogcontent")
+with open("blog/" + name + "/title.txt", "w") as cf:
+    cf.write("blogtitle")
+with open("blog/" + name + "/problems.txt", "w") as cf:
+    cf.write("NOPROBLEMS")
+with open("blog/" + name + "/extraproblems.txt", "w") as cf:
+    cf.write("NOEXTRAPROBLEMS")
+with open("blog/" + name + "/date.txt", "w") as cf:
+    cf.write(str(datetime.now()))
+
