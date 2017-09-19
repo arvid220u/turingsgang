f7f29599223629dc18d8b11c550c8d7da90c9208
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2017-09-19 22:37:23 +0000

    Add intenuddaschack problem

---
+++ problems/intenuddaschack/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    if (N % 2 == 0) {
+        cout << ":(" << endl;
+    } else {
+        cout << ":)" << endl;
+    }
+
+    return 0;
+}
