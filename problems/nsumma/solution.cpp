6130cef4619f5521029c489e756aa9c3448eb7f8
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2017-09-25 14:48:39 +0000

    Update solutions for clarity

--- problems/nsumma/solution.cpp
+++ problems/nsumma/solution.cpp
@@ -2,13 +2,28 @@
 using namespace std;
 
 int main() {
-
-    int n, total = 0, temp;
+    
+    // Skapa variabeln n
+    int n;
+    
+    // Läs in talet n
     cin >> n;
-    for (int i = 0; i != n; ++i) {
-        cin >> temp;
-        total += temp;
+    
+    // Skapa en variabel för summan, och sätt den till 0
+    int summa;
+    summa = 0;
+    
+    // Loopa n gånger
+    for (int i = 0; i < n; i++) {
+        // Läs in nästa tal
+        int x;
+        cin >> x;
+        // Öka summan med det här talet
+        summa = summa + x;
     }
-    cout << total << endl;
+    
+    // Skriv ut summan
+    cout << summa << endl;
+    
     return 0;
 }
--- problems/tomatkop/solution.cpp
+++ problems/tomatkop/solution.cpp
@@ -4,13 +4,21 @@ using namespace std;
 int main() {
 
     int X, K, T;
+    // På det här sättet kan vi läsa in 3 variabler genom att bara använda en rad.
     cin >> X >> K >> T;
+    // Vi hade också kunnat skriva följande:
+    // cin >> X;
+    // cin >> K;
+    // cin >> T;
+    // Det betyder exakt samma sak, men tar lite längre tid att skriva.
 
     int totalkostnad = X * T;
 
     if (totalkostnad <= K) {
+        // Notera: skulle vi skriva ut "Ja!" eller "ja" skulle vi få fel svar (Wrong Answer).
         cout << "JA" << endl;
     } else {
+        // På samma sätt om vi skulle skriva ut "nä" eller "nEJ". Datorn är petig.
         cout << "NEJ" << endl;
     }
 
